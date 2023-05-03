3a9ee95cc89befaf7a4239f20f9faa97769124f0
MustafaKcdm <mmustafakocaadam@gmail.com>
2023-05-04 02:30:56 +0300

    c4 finished end

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.CucumberOptions;
         //tags = "@SmokeTest and @Regression", // and  olduğu zaman iki etiketin aynı anda bulunması gerekir çalışması için
         tags = "@SmokeTest or @Regression", //smoketest ve regression etiketli featurelar çalışır
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"json:target/cucumber/cucumber.json"} //jenkinsde cucumber raporu için eklendi
+
 )
 
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
